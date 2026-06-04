bd2700ad4c28e380d9c1813693a318073893a891
Wéber Márton <webermarton@gmail.com>
2026-06-04 11:43:10 +0200

    Update +layout.ts

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -7,7 +7,7 @@ export const load = async () => {
 	if (browser) {
 		posthog.init('phc_4XB2LcqTutCvDbtvimDaGsOt04nsZ4f23SdJZbDDdEZ', {
 			api_host: 'https://eu.i.posthog.com',
-			defaults: '2025-05-24',
+			defaults: '2026-01-30',
 			person_profiles: 'always'
 		});
 	}
