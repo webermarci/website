5a4845c9b8533bf40659c55af8b5b81b4b588b5e
Wéber Márton <webermarton@gmail.com>
2026-09-04 10:12:45 +0200

    Defer PostHog initialization until browser idle time

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,41 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+
+	onMount(() => {
+		let cancelled = false;
+
+		const initializePostHog = () => {
+			void import('posthog-js/dist/module.slim').then(({ default: posthog }) => {
+				if (cancelled) return;
+
+				posthog.init('phc_4XB2LcqTutCvDbtvimDaGsOt04nsZ4f23SdJZbDDdEZ', {
+					api_host: 'https://eu.i.posthog.com',
+					defaults: '2026-01-30',
+					person_profiles: 'always'
+				});
+			});
+		};
+
+		let timeoutId: number | undefined;
+		let idleCallbackId: number | undefined;
+
+		if ('requestIdleCallback' in window) {
+			idleCallbackId = window.requestIdleCallback(initializePostHog, { timeout: 2000 });
+		} else {
+			timeoutId = window.setTimeout(initializePostHog);
+		}
+
+		return () => {
+			cancelled = true;
+
+			if (idleCallbackId !== undefined) window.cancelIdleCallback(idleCallbackId);
+			if (timeoutId !== undefined) window.clearTimeout(timeoutId);
+		};
+	});
 </script>
 
 <svelte:head>
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,16 +1,1 @@
-import { browser } from '$app/environment';
-import posthog from 'posthog-js';
-
 export const prerender = true;
-
-export const load = async () => {
-	if (browser) {
-		posthog.init('phc_4XB2LcqTutCvDbtvimDaGsOt04nsZ4f23SdJZbDDdEZ', {
-			api_host: 'https://eu.i.posthog.com',
-			defaults: '2026-01-30',
-			person_profiles: 'always'
-		});
-	}
-
-	return;
-};
